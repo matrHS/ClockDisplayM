31f03d68aa14caafa795a9d8464780f9ce793dd7
matrh88 <57237495+matrh88@users.noreply.github.com>
2023-09-06 09:52:27 +0200

    Added static method for formatting time string. Added static method for incrementing timer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,68 @@ package org.example;
  * Main method.
  */
 public class Main {
+
+  /**
+   * Takes in two NumberDisplay objects and returns a string formatted like a digital clock.
+   *
+   * @param hours NumberDisplay object to represent hours.
+   * @param minutes NumberDisplay object to represent minutes.
+   * @return String value in format **:**
+   */
+  private static String getTimeAsString(NumberDisplay hours, NumberDisplay minutes) {
+    String timeAsString;
+
+    timeAsString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
+
+    return timeAsString;
+  }
+
+  /**
+   * Takes in two NumberDisplay objects and returns a string formatted like a digital clock.
+   *
+   * @param hours NumberDisplay object to represent hours.
+   * @param minutes NumberDisplay object to represent minutes.
+   * @param seconds NumberDisplay object to represent seconds.
+   * @return String value in format **:**.**
+   */
+  private static String getTimeAsString(NumberDisplay hours, NumberDisplay minutes, NumberDisplay seconds) {
+    String timeAsString;
+
+    timeAsString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + "." + seconds.getDisplayValue();
+
+    return timeAsString;
+  }
+
+  /**
+   * Increments the clock by time tick.
+   *
+   * @param hours NumberDisplay to represent hours.
+   * @param minutes NumberDisplay to represent minutes.
+   */
+  private static void incrementTime(NumberDisplay hours, NumberDisplay minutes) {
+    minutes.increment();
+    if (minutes.getValue() == 0) {
+      hours.increment();
+    }
+  }
+
+  /**
+   * Increments the clock by time tick.
+   *
+   * @param hours NumberDisplay to represent hours.
+   * @param minutes NumberDisplay to represent minutes.
+   * @param seconds NumberDisplay to represent seconds.
+   */
+  private static void incrementTime(NumberDisplay hours, NumberDisplay minutes, NumberDisplay seconds) {
+    seconds.increment();
+    if (seconds.getValue() == 0) {
+      minutes.increment();
+      if (minutes.getValue() == 0) {
+        hours.increment();
+      }
+    }
+  }
+
   /**
    * Main Method.
    *
@@ -12,19 +74,14 @@ public class Main {
   public static void main(String[] args) {
     NumberDisplay hours = new NumberDisplay(24);
     NumberDisplay minutes = new NumberDisplay(60, 46);
+    NumberDisplay seconds = new NumberDisplay(60, 56);
 
     hours.setValue(19);
+    minutes.setValue(58);
 
     for (int i = 0; i < 500; i++) {
-      System.out.printf(hours.getDisplayValue() + ":" + minutes.getDisplayValue() + "\n");
-      minutes.increment();
-      if (minutes.getValue() == (minutes.getLimit() - 1)) {
-        hours.increment();
-      }
+      System.out.println(getTimeAsString(hours,minutes,seconds));
+      incrementTime(hours,minutes,seconds);
     }
-
-
-
-
   }
 }
